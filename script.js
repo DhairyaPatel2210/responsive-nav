b682ebb1d780ca22479f707cb6dd135f14382472
Dhairya Patel <80202158+DhairyaPatel2210@users.noreply.github.com>
2021-05-07 23:50:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,29 @@ const navSlide = () => {
     })
 }
 
+const callUp = () =>{
+    const contactButton = document.querySelector(".Contact-button")
+    const hiddenDiv = document.querySelectorAll(".contact-links div")
+    const image = document.getElementById("phone-image");
+    contactButton.addEventListener('click',() => {   
+        if(hiddenDiv[0].style.opacity == 1){
+            hiddenDiv.forEach((divs,index) => {
+                divs.style.animation = `linksFadeOut 0.3s`;
+                divs.style.opacity = 0;
+                image.src = "images/icons8-up.svg";
+            })
+            contactButton.style.opacity = 1;
+        }
+        else{
+            hiddenDiv.forEach((divs,index) => {
+                divs.style.animation = `linksFadeIn 0.3s`;
+                divs.style.opacity = 1;
+                image.src = "images/icons8-down.svg";
+            })
+            contactButton.style.opacity = 0.5;
+        }
+    })
+}
 
 navSlide();
+callUp();
